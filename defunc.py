596a0dffabec8460aa3d2f6e2854b931332c4998
unknown <Keqy>
2024-01-10 00:59:54 +0300

    Добавил функцию парсинга user-id. Добавил настройки. Добавил файл userids.txt, options.txt. Файл patricipants.txt заменён на usernames.txt.

--- defunc.py
+++ defunc.py
@@ -1,5 +1,7 @@
 from telethon.tl.functions.channels import InviteToChannelRequest
-
+from telethon.sync import TelegramClient
+import os
+import time
 
 def inviting(client, channel, users):
     client(InviteToChannelRequest(
@@ -8,18 +10,115 @@ def inviting(client, channel, users):
     ))
 
 
-def parsing(client, index):
+def parsing(client, index: int, id: bool, name: bool):
     all_participants = []
     all_participants = client.get_participants(index)
-    with open('participants.txt', 'r+') as f:
-        for user in all_participants:
-            if user.username:
-                if ('Bot' not in user.username) and ('bot' not in user.username):
-                    if (('@' + user.username + '\n') not in f.read()):
-                        f.write('@' + user.username + '\n')
+    if name:
+        with open('usernames.txt', 'r+') as f:
+            usernames = f.readlines()
+            for user in all_participants:
+                if user.username:
+                    if ('Bot' not in user.username) and ('bot' not in user.username):
+                        if (('@' + user.username + '\n') not in usernames):
+                            f.write('@' + user.username + '\n')
+                        else:
+                            continue
                     else:
                         continue
                 else:
                     continue
+    if id:
+        with open('userids.txt', 'r+') as f:
+            userids = f.readlines()
+            for user in all_participants:
+                if (str(user.id) + '\n') not in userids:
+                    f.write(str(user.id) + '\n')
+
+
+def config():
+    while True:
+        os.system('cls||clear')
+
+        with open('options.txt', 'r+') as f:
+            if not f.readlines():
+                f.write("NONEID\n"
+                        "NONEHASH\n"
+                        "True\n"
+                        "True\n")
+                continue
+                
+        options = getoptions()
+        sessions = []
+        for file in os.listdir('.'):
+            if file.endswith('.session'):
+                sessions.append(file)
+
+        key = str(input((f"1 - Обновить api_id [{options[0].replace('\n', '')}]\n"
+                         f"2 - Обновить api_hash [{options[1].replace('\n', '')}]\n"
+                         f"3 - Парсить user-id [{options[2].replace('\n', '')}]\n"
+                         f"4 - Парсить user-name [{options[3].replace('\n', '')}]\n"
+                         f"5 - Добавить аккаунт юзербота[{len(sessions)}]\n"
+                          "6 - Сбросить настройки\n"
+                          "e - Выход\n"
+                          "Ввод: ")
+                    ))
+
+        if key == '1':
+            os.system('cls||clear')
+            options[0] = str(input("Введите API_ID: ")) + "\n"
+
+        elif key == '2':
+            os.system('cls||clear')
+            options[1] = str(input("Введите API_HASH: ")) + "\n"
+
+        elif key == '3':
+            if options[2] == 'True\n':
+                options[2] = 'False\n'
+            else:
+                options[2] = 'True\n'
+
+        elif key == '4':
+            if options[3] == 'True\n':
+                options[3] = 'False\n'
+            else:
+                options[3] = 'True\n'
+        
+        elif key == '5':
+            os.system('cls||clear')
+            if options[0] == "NONEID\n" or options[1] == "NONEHASH":
+                print("Проверьте api_id и api_hash")
+                time.sleep(2)
+                continue
+
+            print("Аккаунты:\n")
+            for i in sessions:
+                print(i)
+
+            phone = str(input("Введите номер телефона аккаунта: "))
+            client = TelegramClient(phone, int(options[0].replace('\n', '')), 
+                                    options[1].replace('\n', '')).start(phone)
+            
+        elif key == '6':
+            os.system('cls||clear')
+            answer = input("Вы уверены?\nAPI_ID и API_HASH будут удалены\n"
+                           "1 - Удалить\n2 - Назад\n"
+                           "Ввод: ")
+            if answer == '1':    
+                options.clear()
+                print("Настройки очищены.")
+                time.sleep(2)
             else:
                 continue
+
+        elif key == 'e':
+            os.system('cls||clear')
+            break
+
+        with open('options.txt', 'w') as f:
+            f.writelines(options)
+
+
+def getoptions():
+    with open('options.txt', 'r') as f:
+        options = f.readlines()
+    return options
--- main.py
+++ main.py
@@ -1,78 +1,144 @@
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
+from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
 from defunc import *
 import time
 import random
-from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
+import os
 
-
-api_id = input('Введите ваш API ID телеграмма')
-api_hash = input('Введите ваш API HASH телеграмма')
-client0 = TelegramClient('client0', api_id, api_hash).start()
-
-selection = int(input('[1] Парсинг\n[2] Инвайтинг\nВвод: '))
-if selection == 1:
-
-    chats = []
-    last_date = None    
-    size_chats = 200
-    groups = []         
-
-    result = client0(GetDialogsRequest(
-        offset_date=last_date,
-        offset_id=0,
-        offset_peer=InputPeerEmpty(),
-        limit=size_chats,
-        hash=0
-    ))
-    chats.extend(result.chats)
-
-    for chat in chats:
-        try:
-            if chat.megagroup is True:
-                groups.append(chat)         
-        except:
+if __name__ == "__main__":
+    while True:
+        options = getoptions()
+        if not options or options[0] == "NONEID\n" or options[1] == "NONEHASH\n":
+            print("Добавьте API_ID и API_HASH")
+            time.sleep(2)
+            config()
             continue
-
-    i = 0
-    print('Очистка базы юзернеймов: -1') 
-    print('-----------------------------')
-    for g in groups:
-        print(str(i) + ' - ' + g.title)
-        i+=1
-    print('||[' + str(i + 1) + ' - ' + 'Спарсить всё' + ']||')
-    g_index = int(input())
-
-    print('Парсим!')
-    if g_index < i + 1:
-        target_group = groups[g_index]
-        parsing(client0, target_group)
-        print('Спарсино.')
-    elif g_index == i + 1:
-        for g_index in groups:
-            parsing(client0, g_index)
-        print('Было трудно, но мы справились)')
-    elif g_index == 1234:
-        f = open('participants.txt', 'w')
-        f.close()
-        print('Очистка выполнена успешно.')
-
-elif selection == 2:
-    with open('participants.txt', 'r') as f:
-        users = list(f)
         
-    channelname = input('Введите имя канала для инвайта (без "@")')
-    for limit in range(20):
-        try:
-            inviting(client0, channelname, users[limit].replace('\n', ''))
-            print(users[limit].replace('\n', ''))
-            time.sleep(random.randrange(15, 40))
-        except UserPrivacyRestrictedError:
-            print('Пользователь ' + users[limit].replace('\n', '') + ' запретил его инвайтить. Пропускаем :(')
-        except PeerFloodError:
-            print('Телеграмм заспамлен. Сворачиваемся.')
-            break
-        except Exception as error:
-            print(error)
+        api_id = int(options[0].replace('\n', ''))
+        api_hash = str(options[1].replace('\n', ''))
+        if options[2] == 'True\n':
+            user_id = True
+        else:
+            user_id = False
+        if options[3] == 'True\n':
+            user_name = True
+        else:
+            user_name = False
+
+        os.system('cls||clear')
+        selection = str(input("1 - Настройки\n"
+                            "2 - Парсинг\n"
+                            "3 - Инвайтинг\n"
+                            "e - Выход\n"
+                            "Ввод: "))
+        
+
+        if selection == '1':
+            config()
+
+
+        elif selection == '2':
+            chats = []
+            last_date = None    
+            size_chats = 200
+            groups = []         
+
+            print("Выберите юзер-бота для парсинга.\n"
+                "(Аккаунт который состоит в группах, которые нужно спарсить)\n")
+
+            sessions = []
+            for file in os.listdir('.'):
+                if file.endswith('.session'):
+                    sessions.append(file)
+
+
+            for i in range(len(sessions)):
+                print(f"[{i}] -", sessions[i], '\n')
+            i = int(input("Ввод: "))
+            
+            client = TelegramClient(sessions[i].replace('\n', ''), api_id, api_hash).start(sessions[i].replace('\n', ''))
+
+            result = client(GetDialogsRequest(
+                offset_date=last_date,
+                offset_id=0,
+                offset_peer=InputPeerEmpty(),
+                limit=size_chats,
+                hash=0
+            ))
+            chats.extend(result.chats)
+
+            for chat in chats:
+                try:
+                    if chat.megagroup is True:
+                        groups.append(chat)         
+                except:
+                    continue
+
+            i = 0
+            print('Очистка базы юзеров: clear') 
+            print('-----------------------------')
+            for g in groups:
+                print(str(i) + ' - ' + g.title)
+                i+=1
+            print(str(i + 1) + ' - ' + 'Спарсить всё')
+            g_index = str(input())
+
+            if g_index == 'clear':
+                f = open('usernames.txt', 'w')
+                f.close()
+                f = open('userids.txt', 'w')
+                f.close
+
+            elif int(g_index) < i + 1:
+                target_group = groups[int(g_index)]
+                parsing(client, target_group, user_id, user_name)
+                print('Спаршено.')
+
+            elif int(g_index) == i + 1:
+                for g_index in groups:
+                    parsing(client, g_index, user_id, user_name)
+                print('Спаршено.')
+
+            
+
+        elif selection == '3':
+            with open('usernames.txt', 'r') as f:
+                users = list(f)
+
+            print("Выберите юзер-бота для инвайтинга.\n"
+                "(Аккаунт который состоит в группе, в которую производится инвайт)")
+            
+            sessions = []
+            for file in os.listdir('.'):
+                if file.endswith('.session'):
+                    sessions.append(file)
+
+            for i in range(len(sessions)):
+                print(f"{i} -", sessions[i])
+            i = int(input("Ввод: "))
+            
+            client = TelegramClient(sessions[i].replace('\n', ''), api_id, api_hash)
+
+            channelname = input('Введите имя канала для инвайта (без "@")')
+
+            for limit in range(20):
+                try:
+                    inviting(client, channelname, users[limit].replace('\n', ''))
+                    print(users[limit].replace('\n', ''))
+                    time.sleep(random.randrange(15, 40))
+
+                except UserPrivacyRestrictedError:
+                    print('Пользователь ' + users[limit].replace('\n', '') + ' запретил его инвайтить. Пропускаем :(')
+
+                except PeerFloodError:
+                    print('Телеграмм заспамлен.')
+                    break
+
+                except Exception as error:
+                    print(error)
+                    break
+
+        elif selection == 'e':
             break
